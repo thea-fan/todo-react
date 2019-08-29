297b282ce20c96a397f0512e122918eb5ff42532
thea-fan <julythefourteen@gmail.com>
2019-08-29 14:45:31 +0800

    input validation implemented

--- script.jsx
+++ script.jsx
@@ -4,23 +4,48 @@ class List extends React.Component {
 
     this.state = {
       word : "",
-      list : []
+      list : [],
+      className : "",
+      warning: ""
     }
   }
 
   addItem(){
+     var pass = {
+        className: "",
+        warning: ""
+    }
+
     var item = this.state.word;
-    var updateList = this.state.list;
-    updateList.push(item);
-    this.setState({list:updateList});
+    if (item.length > 0){
+        var updateList = this.state.list;
+        updateList.push(item);
+        this.setState({list:updateList});
+        this.state.word = "";
+        this.setState(pass)
+    } else {
+        this.setState({className: "warning", warning: "Pls enter smth"})
+    }
   }
 
   changeHandler(event){
-    if (event.keyCode === 13){
-        this.addItem();
+    var pass = {
+        className: "",
+        warning: ""
+    }
+    var input = event.target.value;
+    if (input.length <= 25){
+        this.setState(pass)
     } else {
-        var input = event.target.value;
-        this.setState({word:input});
+        this.setState({className: "warning", warning: "Max 25 characters only"})
+    }
+
+    this.setState({word:input});
+  }
+
+  enterHandler(event){
+     if (event.keyCode === 13){
+        this.addItem();
     }
   }
 
@@ -32,8 +57,9 @@ class List extends React.Component {
 
       return (
         <div className="list">
-            <input type="text" onChange={(event)=>{this.changeHandler(event)}} onKeyDown = {(event)=>{this.changeHandler(event)}} value={this.state.word}/>
+            <input type="text" onChange={(event)=>{this.changeHandler(event)}} onKeyDown = {(event)=>{this.enterHandler(event)}} value={this.state.word} className = {this.state.className}/>
             <button onClick={()=>{this.addItem()}}>add item</button>
+            <p className = "warning-text">{this.state.warning}</p>
             <div>
                 <p>*******</p>
                 {addToList}
